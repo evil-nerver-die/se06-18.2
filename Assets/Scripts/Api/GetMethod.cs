17b9ba7ac0045d13d2f6af57df4075bd3f93ed6f
Son NM <--kienthanh2seri@gmail.com-->
2022-01-22 23:20:52 +0700

    fix error display

--- Assets/Scripts/Api/GetMethod.cs
+++ Assets/Scripts/Api/GetMethod.cs
@@ -10,6 +10,9 @@ public class GetMethod : MonoBehaviour
     public static string urlUser = "https://lmh-json-api.herokuapp.com/users";
     public static string jsonResponseUser = ApiHelper.getApiData(urlUser);
     public static JSONNode itemsDataUser = JSON.Parse(jsonResponseUser);
+    public int userId = 0;
+    public int id = 0;
+    public bool valid = true;
     TextMeshProUGUI outputArea;
     // Start is called before the first frame update
     void Start()
@@ -33,14 +36,23 @@ public class GetMethod : MonoBehaviour
                 }
             }
         }
-        for (int i = 0; i < 8;i++) {
+        int index = 0;
+        if (count < 8) {
+            index = count;
+        }else {
+            index = 8;
+        }
+        if(count >  itemsDataUser.AsArray.Count) {
+            itemsDataUser[count-1]["id"] = count;
+            itemsDataUser[count-1]["name"] = ReadText.getUsername();
+        }
+        for (int i = 0; i < index;i++) {
             foreach (JSONNode layerNode in itemsDataUser.AsArray) {
                 if(itemsData[i]["userId"] == layerNode["id"]) {
                     coinText += "Name : " + layerNode["name"] + "  -  score : " + itemsData[i]["count"] + "\n";
                 }
             }  
         }
-
         outputArea.text = coinText;
         Debug.Log(coinText);
     }
