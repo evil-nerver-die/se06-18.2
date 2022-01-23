75c62718a6737b9a6b81b5d48b7a4fd818411dc8
Son NM <--kienthanh2seri@gmail.com-->
2022-01-23 10:04:42 +0700

    update display coins

--- Assets/Scripts/Api/GetMethod.cs
+++ Assets/Scripts/Api/GetMethod.cs
@@ -7,14 +7,8 @@ using TMPro;
 
 public class GetMethod : MonoBehaviour
 {
-    public static string urlUser = "https://lmh-json-api.herokuapp.com/users";
-    public static string jsonResponseUser = ApiHelper.getApiData(urlUser);
-    public static JSONNode itemsDataUser = JSON.Parse(jsonResponseUser);
-    public int userId = 0;
-    public int id = 0;
-    public bool valid = true;
     TextMeshProUGUI outputArea;
-    // Start is called before the first frame update
+
     void Start()
     {
         outputArea = GameObject.Find("Output").GetComponent<TextMeshProUGUI>();
@@ -23,6 +17,9 @@ public class GetMethod : MonoBehaviour
 
     public IEnumerator getData() {
         yield return new WaitForSeconds(1);
+        string urlUser = "https://lmh-json-api.herokuapp.com/users";
+        string jsonResponseUser = ApiHelper.getApiData(urlUser);
+        JSONNode itemsDataUser = JSON.Parse(jsonResponseUser);
         string jsonResponse = ApiHelper.getApiData("https://lmh-json-api.herokuapp.com/coins");
         JSONNode itemsData = JSON.Parse(jsonResponse);
         int count = itemsData.AsArray.Count;
@@ -41,10 +38,6 @@ public class GetMethod : MonoBehaviour
             index = count;
         }else {
             index = 8;
-        }
-        if(count >  itemsDataUser.AsArray.Count) {
-            itemsDataUser[count-1]["id"] = count;
-            itemsDataUser[count-1]["name"] = ReadText.getUsername();
         }
         for (int i = 0; i < index;i++) {
             foreach (JSONNode layerNode in itemsDataUser.AsArray) {
